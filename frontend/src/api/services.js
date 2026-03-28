b3c4aac1b80a94c549569f3cdb16988b98316f8f
Sarthi <workwithsarthi@gmail.com>
2026-03-29 00:20:10 +0530

    changes in services.js, added api infront

--- frontend/src/api/services.js
+++ frontend/src/api/services.js
@@ -1,8 +1,12 @@
 import api from "./axiosInstance";
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
+  withCredentials: true,
+});
 
 // ─── Auth ────────────────────────────────────────────────
 export const authAPI = {
-  googleLogin: (idToken) => api.post("api/auth/google", { idToken }),
+  googleLogin: (idToken) => api.post("/auth/google", { idToken }),
   getMe: () => api.get("/auth/me"),
 };
 
