89fa40589abf9c78ae3389bfaa6345b186c26db8
Sarthi <workwithsarthi@gmail.com>
2026-03-29 00:04:14 +0530

    changes cors policy

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,20 @@ connectDB();
 
 // Security middleware
 app.use(helmet());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://courseify-blond.vercel.app"
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
